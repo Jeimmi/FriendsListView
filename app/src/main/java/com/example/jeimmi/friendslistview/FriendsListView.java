944ffcaeff5d65405f2def06e778739d8de21f18
Jeimmi <jeimmi.gomez@gmail.com>
2016-05-04 11:39:35 -0700

    first commit

---
+++ app/src/main/java/com/example/jeimmi/friendslistview/FriendsListView.java
@@ -0,0 +1,13 @@
+package com.example.jeimmi.friendslistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendsListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_list_view);
+    }
+}
