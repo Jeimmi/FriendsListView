a9ef9cf1e375f4b1fc319d74224db4185519a9a3
Jeimmi <jeimmi.gomez@gmail.com>
2016-05-04 11:51:42 -0700

    Implemented a simple listview of friends

--- app/src/main/java/com/example/jeimmi/friendslistview/FriendsListView.java
+++ app/src/main/java/com/example/jeimmi/friendslistview/FriendsListView.java
@@ -2,12 +2,21 @@ package com.example.jeimmi.friendslistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class FriendsListView extends AppCompatActivity {
-
+    String [] friends = {"Dennis","David","GG"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_list_view);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_adapter, friends);
+
+        ListView listView = (ListView) findViewById(R.id.friend_lv);
+        listView.setAdapter(adapter);
+
+
     }
 }
